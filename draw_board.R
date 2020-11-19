11a1efa083e609e1cdc998860f9dad979d4d56ad
Pin-Chun Chen <pinchuc@uci.edu>
2020-11-19 10:54:16 -0800

    draw_board

--- draw_board.R
+++ draw_board.R
@@ -8,15 +8,22 @@ board_size <- 19
 tile_color <- c("white","black")
 i = 1
 
-df <- data.frame(x = 0:board_size, y = 0:board_size)
+df <- data.frame(x = 1:board_size, y = 1:board_size)
 
 back <- ggplot(df) +
   geom_point(aes(x, y), size = 5, alpha = 0) + 
   theme(
+    aspect.ratio = 1,
+    axis.ticks = element_line(colour = "black", size = 1),
     panel.grid.major = element_line(colour = "black"),
     panel.grid.minor = element_line(size = 1, colour="black"),
     panel.background = element_rect(fill = "#D2B48C"),
-    panel.ontop = FALSE) 
+    panel.ontop = FALSE) + 
+  scale_x_continuous(breaks= seq(1,  board_size, by=1),
+                     minor_breaks = seq(1, board_size, 1)) + 
+  scale_y_continuous(breaks= seq(1,  board_size, by=1),
+                     minor_breaks = seq(1, board_size, 1))
+
 
 #each iteration
 back +
---
+++ functions.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+# Draws the board
+gomoku_board <- function(size, color) {
+    require(ggplot2)
+
+    # Choose whether the board is 15x15 or 19x19
+
+
+}
+
+
+# Places a piece
+gomoku_move <- function(x, y, color) {
+
+    # checking missing arguments
+    if (missing(x))
+        stop("Need to specify x-coordinate of the move.")
+
+    if (missing(y))
+        stop("Need to specify y-coordinate of the move.")
+
+    }
+
+# Checks for victory
+gomuku_victory <- function() {
+
+    if (missing())
+
+    paste0(color, "has won.", sep = "")
+
+    }
+
+
+
+
