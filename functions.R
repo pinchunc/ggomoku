7937b53a16604c8aaeff5672ffed3ab2c17d3025
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-11-19 02:09:46 -0800

    Adding comments

--- functions.R
+++ functions.R
@@ -1,17 +1,25 @@
 library(tidyverse)
 
 # Draws the board
-gomoku_board <- function(size, color) {
+gomoku.board <- function(size, color) {
+
     require(ggplot2)
 
-    # Choose whether the board is 15x15 or 19x19
+    if (missing(size))
+        stop("Size must be specified ")
 
+    df <- data.frame(x = 1:size, y = 1:size)
+    # Choose whether the board is 15x15 or 19x19
 
 }
 
 
+
+
+
+
 # Places a piece
-gomoku_move <- function(x, y, color) {
+gomoku.move <- function(x, y, color) {
 
     # checking missing arguments
     if (missing(x))
@@ -20,17 +28,25 @@ gomoku_move <- function(x, y, color) {
     if (missing(y))
         stop("Need to specify y-coordinate of the move.")
 
+    if (missing(color))
+        stop("Need to specify whether you are placing a black or white piece.")
+
+    # if color = black
+    #
+
     }
 
 # Checks for victory
-gomuku_victory <- function() {
+gomuku.victory <- function() {
 
-    if (missing())
+    #if (missing())
 
     paste0(color, "has won.", sep = "")
 
     }
 
 
-
+# Binary values in a matrix (1 = white, 0 = black)
+# Cannot place a piece where there already is a piece (is.na())
+# Cannot place two of the same color in a row.
 
