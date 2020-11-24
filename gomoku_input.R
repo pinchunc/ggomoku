aaef892a38eb37f90ac55e15b72d1793748ab941
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-11-24 15:00:27 -0800

    Adding board_size argument to gomoku_input()

--- gomoku_input.R
+++ gomoku_input.R
@@ -1,16 +1,17 @@
-gomoku_input <- function() {
+gomoku_input <- function(board_size) {
 
-    input <- readline(prompt = "Enter x coordinate of tile (integers only): ")
+
+    input <- as.integer(readline(prompt = "Enter x coordinate of tile (integers only): "))
 
     # Check that the user put in an actual number (as.integer would coerce it to NA otherwise)
     if (is.na(input)) {
         message("That is not a valid input. Please select whole numbers between 1 and ", board_size, ".")
-        return(gomoku_input())
+        return(gomoku_input(board_size))
     }
     # Check that the move is within the confines of the board
     if (input > board_size) {
         message("That value is not on this board. Please select whole numbers between 1 and ", board_size, ".")
-        return(gomoku_input())
+        return(gomoku_input(board_size))
     }
 
     return(as.integer(input))
