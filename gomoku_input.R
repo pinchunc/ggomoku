1c9273154171bbb2e1aa982de3928239fb5ccd28
J Steven Raquel <jsteven.raquel8@gmail.com>
2020-11-24 15:57:59 -0800

    Changing prompt to simply prompt for general coordinate.

--- gomoku_input.R
+++ gomoku_input.R
@@ -1,7 +1,7 @@
 gomoku_input <- function(board_size) {
 
 
-    input <- as.integer(readline(prompt = "Enter x coordinate of tile (integers only): "))
+    input <- as.integer(readline(prompt = "Enter coordinate of tile: "))
 
     # Check that the user put in an actual number (as.integer would coerce it to NA otherwise)
     if (is.na(input)) {
@@ -9,7 +9,7 @@ gomoku_input <- function(board_size) {
         return(gomoku_input(board_size))
     }
     # Check that the move is within the confines of the board
-    if (input > board_size) {
+    if (!(input %in% 1:board_size)) {
         message("That value is not on this board. Please select whole numbers between 1 and ", board_size, ".")
         return(gomoku_input(board_size))
     }
