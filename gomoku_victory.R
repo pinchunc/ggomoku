d0bfc185a5ecd5bfad71f6e6ccce50e8b3dedb6f
Pin-Chun Chen <pinchuc@uci.edu>
2020-11-24 13:31:45 -0800

    check victory for diagonal and reverse diagonal using the map function (remove for loop)

--- gomoku_victory.R
+++ gomoku_victory.R
@@ -1,6 +1,6 @@
 
 # test matrix
-matrix <- matrix(nrow = 19, ncol = 19)
+matrix <- matrix(1:19^2, nrow = 19, ncol = 19)
 
 # Assigning a test vector to one row and one column of the matrix
 test <- c("black", "white", "white", "white", "white", "white", "black")
@@ -12,71 +12,89 @@ revdiag(matrix)[1:7] <- test
 board_size <- nrow(board$data)
 
 gomoku_victory <- function(matrix) {
-  require(lava)
   # Pull out a column or a row from the matrix as a vector
   winner <- NA
-  # checking for winner on rows
-  rows <- apply(matrix, MARGIN = 1, FUN = rle)
 
-  for (i in 1:nrow(matrix)) {
-    if (5 %in% rows[[i]]$lengths) {
-      winner <- rows[[i]]$values[which(rows[[i]]$lengths == 5)]
-      break
-    }
-    else {
-      next
+  # checking for winner on rows
+  if (is.na(winner)) {
+    rows <- apply(matrix, MARGIN = 1, FUN = rle)
+    for (i in 1:nrow(matrix)) {
+      if (5 %in% rows[[i]]$lengths) {
+        winner <- rows[[i]]$values[which(rows[[i]]$lengths == 5)]
+        break
+      }
+      else {
+        next
+      }
     }
   }
+  else {
+    return(winner)
+    }
 
   # checking for winner on columns
-  cols <- apply(matrix, MARGIN = 2, FUN = rle)
-
-  for (j in 1:ncol(matrix)) {
-    if (5 %in% cols[[j]]$lengths) {
-      winner <- cols[[j]]$values[which(cols[[j]]$lengths == 5)]
-      break
-    }
-    else {
-      next
-    }
-  }
-
-  # checking for winner on diagonals
-  # split the matrix to get diagonals
-  d <- row(matrix) - col(matrix)
-  diags_num <- split(matrix, d)
-  diags_rle <- lapply(diags_num, FUN = rle)
-  for (i in 1:(length(diags_rle))) {
-    if (5 %in% diags_rle[i]$lengths) {
-      winner <- diags_rle[i]$values[which(diags_rle[i]$lengths == 5)]
-      break
-    }
-    else {
-      next
+  if (is.na(winner)) {
+    cols <- apply(matrix, MARGIN = 2, FUN = rle)
+    for (i in 1:nrow(matrix)) {
+      if (5 %in% rows[[i]]$lengths) {
+        winner <- rows[[i]]$values[which(rows[[i]]$lengths == 5)]
+        break
+      }
+      else {
+        next
+      }
     }
   }
-
-  # split the matrix to get reverse diagonals
-  # revs <- rle(revdiag(matrix))
-  # if (5 %in% revs$lengths) {
-  #   winner <- revs$values[which(revs$lengths == 5)]
-  # }
-  r <- row(matrix) + col(matrix)
-  rev_diags_num <- split(matrix, r)
-  rev_diags_rle <- lapply(rev_diags_num, FUN = rle)
-  for (i in 1:(length(rev_diags_rle))) {
-    if (5 %in% rev_diags_rle[i]$lengths) {
-      winner <- rev_diags_rle[i]$values[which(rev_diags_rle[i]$lengths == 5)]
-      break
-    }
-    else {
-      next
-    }
+  else {
+    return(winner)
   }
   
-  return(winner)
+  # checking for winner on diagonals
+  # split the matrix to get diagonals
+  if (is.na(winner)) {
+    d <- row(matrix) - col(matrix)
+    diags_num <- split(matrix, d)
+    diags_rle <- lapply(diags_num, FUN = rle)
+    winner_list <- map(diags_rle, gomoku_winner)
+    if ('white' %in% winner_list){
+      winner <- "white"
+    }
+    else if ('black' %in% winner_list){
+      winner <- "black"
+    }
+    else {
+      winner <- NA
+    }
+  }
+  else {
+    return(winner)
+  }
+  
+  # split the matrix to get reverse diagonals
+  if (is.na(winner)) {
+    r <- row(matrix) + col(matrix)
+    rev_diags_num <- split(matrix, r)
+    rev_diags_rle <- lapply(rev_diags_num, FUN = rle)
+    winner_list <- map(rev_diags_rle, gomoku_winner)
+    if ('white' %in% winner_list){
+      winner <- "white"
+    }
+    else if ('black' %in% winner_list){
+      winner <- "black"
+    }
+    else {
+      winner <- NA
+    }
+  }
+  else {
+    return(winner)
+  }
 }
 
 # Play the game
-
-gomoku_play(gomoku_board())
+gomoku_winner <- function(rle_output) {
+  if (5 %in% rle_output$lengths) {
+    winner_list <- rle_output$values[which(rle_output$lengths == 5)]}
+  else {winner_list <- NA}
+  return(winner_list)
+}
