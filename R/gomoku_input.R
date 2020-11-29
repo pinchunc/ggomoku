0a217bb7e4b4229ade37ad34dcc8efb466085e20
Pin-Chun Chen <pinchuc@uci.edu>
2020-11-29 12:34:02 -0800

    remove redundant if statement for checking NAs

--- R/gomoku_input.R
+++ R/gomoku_input.R
@@ -11,20 +11,14 @@
 #' gomoku_input(board_size = 19)
 #' # When called during agame that used gomoku_board(15)
 #' gomoku_input(board_size = 15)
+
 gomoku_input <- function(board_size) {
-
-
     input <- as.integer(readline(prompt = "Enter coordinate of tile: "))
-
     # Check that the user put in an actual number (as.integer would coerce it to NA otherwise)
-    if (is.na(input)) {
-        message("That is not a valid input. Please select whole numbers between 1 and ", board_size, ".")
-        return(gomoku_input(board_size))
-    }
     # Check that the move is within the confines of the board
     if (!(input %in% 1:board_size)) {
         message("That value is not on this board. Please select whole numbers between 1 and ", board_size, ".")
-        return(gomoku_input(board_size))
+        return(NA)
     }
 
     else {
